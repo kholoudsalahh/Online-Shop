d24cf3a2e11e3beff9811f662c77a3dcf7ed0d9b
kholoudsalah <kholoud.salahhh@gmail.com>
2019-12-09 20:37:32 +0200

    Add all my files

---
+++ project1/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(project1.Startup))]
+
+namespace project1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
